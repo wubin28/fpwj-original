60b3f8f225be9511f16898a1bee69be1ba26b22f
tuna_C24C65CC60 <tuna@cyber-dojo.org>
2016-02-08 12:51:08 +0000

    6

--- sandbox/ProfitTest.java
+++ sandbox/ProfitTest.java
@@ -2,8 +2,8 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ProfitTest {
-    @Test
-    public void should() {
+    //@Test
+    //public void should() {
         assertEquals(1, 1);
-    }
+    //}
 }
